690d2a987e060a8f214fbaae2a49e8ad07dddd52
Abhishek <abhishekchauhan46396@gmail.com>
2024-07-01 01:27:09 +0530

    Add some functionality and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-
+import "./App.css"
 function App() {
   const[todos,setTodos]=useState([])
   const[task,setTask]=useState("")
@@ -8,15 +8,9 @@ const[togglebtn,setTogglebtn]=useState(true)
 const[edit,setEdit]=useState(null)
 const[search,setSearch]=useState("")
 const[searchResult,setSearchresult]=useState(null)
-const[error,setError]=useState(null)
+const[searchDate,setSearchDate]=useState('')
   const handleSubmit=()=>{
-  if(!task){
-    setError("please fill task")
-    return;
-  }else if(!date){
-    setError("please fill date")
-    return
-  }
+
     if(task,date && !togglebtn){
       setTodos(todos.map((element)=>{
        if(element.id===edit){
@@ -32,11 +26,9 @@ const[error,setError]=useState(null)
     else{
       const inputdata = { id: new Date().getTime().toString(), task, date };
     setTodos([...todos,inputdata])
-
     setTask('')
     setDate(' ')
   }
-  setError(null)
     }
   const deletedata=(id)=>{
   const updateitem=todos.filter((val,index)=>{
@@ -53,53 +45,87 @@ return val.id!==id
     setDate(newData.date)
     setEdit(id)
   }
-const SearchData=(task)=>{
-    let searchdata=todos.find((element)=>{
-   return element.task===task;
-    })
-    setSearchresult(searchdata)
+  const SearchData = () => {
+    let searchResults = todos.filter((element) => {
+      return element.task.toLowerCase().includes(search.toLowerCase());
+    });
+
+    setSearchresult(searchResults);
+    setTogglebtn(false);
+  };
+
+const cancelSearch=()=>{
+    setSearchresult(null);
+    setSearch('');
+    setSearchDate('');
+    setTogglebtn(true)
 }
 
 
-const getTodayDate = new Date().toISOString().split('T')[0]
   return (
     <div>
       <div className="container">
        <h2>Todo-List</h2>
   
   <button  button type="button" className="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Add Task</button>
-
-  <input type="search" onChange={(e)=>setSearch(e.target.value)} value={search} />
-  <button onClick={()=>SearchData(search)}>search</button>
-  
-             {searchResult ? (
-               <ul>
-               <li className="list-group-item" >
-              {searchResult.task}- {searchResult.date}
-             
-              <button className="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown"><i className="bi bi-three-dots-vertical"></i>
-             </button>
-             <ul className="dropdown-menu">
-             <li><button onClick={()=>editData(searchResult.id)} className="btn btn-info btn-sl"  data-toggle="modal" data-target="#myModal">Edit</button></li>
-           <li><button className="btn btn-info btn-sl"  onClick={()=>deletedata(searchResult.id)}>Delete</button></li>
-          </ul>
-            </li>
-            </ul>
-             ):(todos.map((todo)=>{
-            return <ul>
-               <li className="list-group-item" >
-              {todo.task}- {todo.date}
-             
-              <button className="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown"><i className="bi bi-three-dots-vertical"></i>
-             </button>
-             <ul className="dropdown-menu">
-             <li><button onClick={()=>editData(todo.id)} className="btn btn-info btn-sl"  data-toggle="modal" data-target="#myModal">Edit</button></li>
-           <li><button className="btn btn-info btn-sl"  onClick={()=>deletedata(todo.id)}>Delete</button></li>
-  </ul>
-            </li>
-            </ul>
-          }))
+  <div className="btn-search">
+        <input
+          type="search"
+          className="form-control"
+          placeholder="Search task"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}/>
+        {togglebtn ? 
+          <button   className="btn btn-Primary"onClick={SearchData}>Search</button>
+         : <button className="btn btn-Secondary"onClick={cancelSearch}>
+            Cancel
+          </button>
         }
+            </div>
+    <ul className='list-groups'>
+  <li className='list-items'>Task Name</li>
+  <li className='list-items'>Task Date</li>
+  <li className='list-items'>Action</li>
+</ul>
+             {searchResult ? (
+               <ul className='list-group'>
+               {searchResult.map((result)=>{
+               return <li className="list-group-item" >
+              <span>{result.task}</span>
+               <sanp>{result.date}</sanp>
+             
+          <li><button className="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown"><i className="bi bi-three-dots-vertical"></i>
+             </button>
+             <ul className="dropdown-menu">
+             <li><button onClick={()=>editData(result.id)} className="btn btn-info btn-sl"  data-toggle="modal" data-target="#myModal">Edit</button></li>
+           <li><button className="btn btn-info btn-sl" onClick={()=>deletedata(result.id)}>Delete</button></li>
+          </ul>
+          </li> 
+            </li>
+               })
+               }
+            </ul>
+             
+             ):(
+              <ul className='list-group'>
+              {todos.map((todo)=>{
+              return <li className='list-group-item'>
+                 <span>{todo.task}</span><span>{todo.date}</span>
+   
+         <li> <button className="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">
+          <i className="bi bi-three-dots-vertical"></i>
+             </button>
+        <ul className="dropdown-menu">
+     <li><button onClick={()=>editData(todo.id)} className="btn btn-info btn-sl"  data-toggle="modal" data-target="#myModal">Edit</button></li>
+        <li><button className="btn btn-info btn-sl"  onClick={()=>deletedata(todo.id)}>Delete</button></li>
+         </ul>
+</li>
+</li>
+          
+              })}
+            </ul>
+              
+        )}
         
 
   <div className="modal fade" id="myModal" role="dialog">
@@ -113,10 +139,10 @@ const getTodayDate = new Date().toISOString().split('T')[0]
         </div>
         <div className="modal-body">
             <label>Task</label><br/>
-            <input type="text" name="task" className="" onChange={(e)=>setTask(e.target.value)} value={task}/>
+            <input type="text" name="task" className="" onChange={(e)=>setTask(e.target.value)} value={task} required/>
             <br/>
             <label>Date</label><br/>
-            <input type="date" name="date" min={getTodayDate}  onChange={(e)=>setDate(e.target.value)} value={date}/>
+            <input type="date" name="date" min={new Date().toISOString().split('T')[0]}  onChange={(e)=>setDate(e.target.value)} value={date} required/>
             <br/>
         </div>
         <div className="modal-footer">
